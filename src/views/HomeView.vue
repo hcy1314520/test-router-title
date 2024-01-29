749536c415d164edb85b597016e3d7fd9e23ceab
Huang Chaoyong <996851388@qq.com>
2024-01-29 11:25:14 +0800

    feat: add test code

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <h3 @click="toLink">to about</h3>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const toLink = () => {
+  router.push({
+    path: '/about',
+    query: {
+      a: '12 44'
+    }
+  })
+}
+</script>
